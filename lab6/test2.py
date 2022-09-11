b898fefcb5ebdff1c56ac61b7f0b0971778eb6ce
vitoriastavis <vitoriastavisa@gmail.com>
2022-09-11 13:56:03 -0300

    yellowasda

--- lab6/test2.py
+++ lab6/test2.py
@@ -166,14 +166,15 @@ for filename in test_images:
     hsv_img = cv.cvtColor(image, cv.COLOR_BGR2HSV)
     cor = np.uint8([[[255, 217, 15]]])
     cor_hsv = cv.cvtColor(cor, cv.COLOR_RGB2HSV)
-     
+   
+  
     light = np.array([20,100,50])
     dark = np.array([40,255,255])
     mask = cv.inRange(hsv_img, light, dark)
     nome = str(i)
     target = cv.bitwise_and(hsv_img, hsv_img, mask = mask)  
     
-    plt.imsave(nome+'.bmp', target)    
+    plt.imsave(nome+'.jpg', mask)    
     
     i = i + 1
 
@@ -195,7 +196,7 @@ for filename in valid_images:
     nome = str(i)
     target = cv.bitwise_and(hsv_img, hsv_img, mask = mask)  
     
-    plt.imsave(nome+'.bmp', target)
+    plt.imsave(nome+'.jpg', mask)
     
     i = i + 1
 
